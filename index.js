feb11df45f9d5fef560da990e88dac79799ff1b3
copaiement <cole.paiement@gmail.com>
2023-11-17 15:01:24 -0800

    EOD commit

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
 const http = require('http');
+const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 8080;
 
+let indexFile;
+
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('My first server!');
+  res.setHeader('Content-Type', 'text/html');
+  switch (req.url) {
+    case '':
+      res.writeHead(200);
+      res.end(indexFile);
+      break;
+    case '/about':
+      console.log('here');
+      res.writeHead(200);
+      res.end(indexFile);
+      break;
+    case '/contact':
+      res.writeHead(200);
+      res.end(indexFile);
+      break;
+    default:
+      res.writeHead(404);
+      res.end(indexFile);
+  }
 };
 
 const server = http.createServer(requestListener);
 
-server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
-});
+fs.readFile(__dirname + '/index.html')
+  .then(contents => {
+    indexFile = contents;
+    server.listen(port, host, () => {
+      console.log(`Server is running on http://${host}:${port}`);
+    });
+  })
+  .catch(err => {
+    console.error(`Could not read file: ${err}`);
+    process.exit(1);
+  });
+
+// const requestListener = function (req, res) {
+//   fs.readFile(__dirname + '/index.html')
+//     .then(contents => {
+//       res.setHeader('Content-Type', 'text/html');
+//       res.writeHead(200);
+//       res.end(contents);
+//     })
+//     .catch(err => {
+//       res.writeHead(500);
+//       res.end(err);
+//       return;
+//     });
+// };
+
+// const server = http.createServer(requestListener);
+
+// server.listen(port, host, () => {
+//   console.log(`Server is running on http://${host}:${port}`);
+// });
